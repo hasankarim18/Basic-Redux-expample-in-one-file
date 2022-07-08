442ae0c1e7108878546e76ce2984b61ab7823463
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-08 22:01:19 +0600

    Subscription of redux

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ const rootReducer = (state = initialState, action) => {
                 ...state, // always create a copy of the state
                 num: state.num + action.payload
             }
-            break
+        case 'DEC_NUM':
+            return {
+                ...state, // dec
+                num: state.num - action.payload
+            }
         default:
             break;
     }
@@ -32,19 +36,32 @@ const rootReducer = (state = initialState, action) => {
 // sotre
 const store = redux.createStore(rootReducer)
 
-console.log('store.getState() before dispatch:', store.getState())
+//console.log('store.getState() before dispatch:', store.getState())
 
 // Dispatching actions
+
+store.subscribe(() => {
+    console.log('Subscribe: ', store.getState())
+})
 
 store.dispatch({
     type: 'INC_NUM',
 
 })
-console.log('store.getState():', store.getState())
+//console.log('store.getState():', store.getState())
 store.dispatch({
     type: "ADD_NUM",
     payload: 34
 })
 
+setTimeout(() => {
+    store.dispatch({
+        type: 'DEC_NUM',
+        payload: 2
+    })
+}, 2000);
+
+
+
 // console.log('store:', store)
-console.log('store.getState() after dispatch:', store.getState())
+//console.log('store.getState() after dispatch:', store.getState())
